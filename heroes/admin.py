161fc7757a532af30b69fbd24f7dadc5ac46f3ce
samuelcody710 <adeyemis710@gmail.com>
2021-06-07 21:57:01 -0700

    commit all

--- heroes/admin.py
+++ heroes/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Article,Comment
+from .models import Article
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
 	list_display = ('title','author','publish','status')
 	list_filter = ('status', 'created', 'publish', 'author')
-
-
-admin.register(Comment)
